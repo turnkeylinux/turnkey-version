60154a59a683f927216d7ae4b7537894a977cc91
Liraz Siri <liraz@sterilesecurity.com>
2010-08-09 06:03:58 +0300

    passed test: detect version if only one version package installed

--- turnkey-version.py
+++ turnkey-version.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python
 import os
 from os.path import *
+
+import re
 import sys
-import getopt
+import glob
 
 def usage(e=None):
     if e:
@@ -19,11 +21,36 @@ def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
+def parse_changelog(fpath):
+    firstline = file(fpath).readline()
+    m = re.match(r'(\S+) \((.*?)\) (\w+);', firstline)
+    if not m:
+        raise Error("couldn't parse changelog '%s'" % fpath)
+    
+    name, version, dist = m.groups()
+    return name, version, dist
+
 def get_turnkey_version():
     try:
         return file("/etc/turnkey_version").read().strip()
     except IOError:
         pass
+
+    for fpath in glob.glob("/usr/share/doc/turnkey-*"):
+        pkgname = basename(fpath)
+        if pkgname in ('turnkey-pylib', 'turnkey-keyring', 'turnkey-version'):
+            continue
+
+        m = re.match(r'(turnkey-.*?)-([\d\.]+|beta)', pkgname)
+        if not m:
+            continue
+        codename, version = m.groups()
+        dist = parse_changelog(join(fpath, "changelog"))[2]
+
+        if dist == 'turnkey':
+            dist = 'lenny'
+
+        return "%s-%s-%s-x86" % (codename, version, dist)
 
     raise Error("can't detect turnkey version")
     
