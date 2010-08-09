0c0be66d81b90c573e3f24bda5024caf742f95b8
Liraz Siri <liraz@sterilesecurity.com>
2010-08-09 05:32:25 +0300

    minimal scaffolding

--- turnkey-version.py
+++ turnkey-version.py
@@ -1,12 +1,4 @@
 #!/usr/bin/python
-"""Example command template
-
-Options:
-  -q --quiet	don't print anything
-  -f=FOO	print FOO
-  --fatal       fatal error
-
-"""
 import sys
 import getopt
 
@@ -18,33 +10,21 @@ def usage(e=None):
     print >> sys.stderr, __doc__.strip()
     sys.exit(1)
 
+class Error(Exception):
+    pass
+
 def fatal(s):
     print >> sys.stderr, "error: " + str(s)
     sys.exit(1)
 
+def get_turnkey_version():
+    raise Error("can't detect turnkey version")
+    
 def main():
     try:
-        opts, args = getopt.gnu_getopt(sys.argv[1:], 'qf:h', ['fatal'])
-    except getopt.GetoptError, e:
-        usage(e)
-
-    opt_quiet = False
-    for opt, val in opts:
-        if opt == '-h':
-            usage()
-
-        if opt in ('-q', '--quiet'):
-            opt_quiet = True
-
-        elif opt == '-f':
-            print "printing foo: " + val
-
-        elif opt == '--fatal':
-            fatal("fatal condition")
-
-    if not opt_quiet:
-        print "printing args: " + `args`
-
+        print get_turnkey_version()
+    except Error, e:
+        fatal(e)
     
 if __name__=="__main__":
     main()
