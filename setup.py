cf5dc5c90463dc40bd676c0e839b6261c02a5984
Stefan Davis <stefan@turnkeylinux.org>
2019-06-24 01:38:45 +0000

    Migrate from Make to pybuild
    - Remove makefile
    - Add setup.py
    - Remove .py extension from turnkey-version

---
+++ setup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+setup(name='turnkey-version',
+    version='1.0',
+    description='Turnkey Version Reporting Utility',
+    author='Stefan Davis',
+    url='https://github.com/turnkeylinux/turnkey-version',
+    scripts=['turnkey-version'])
