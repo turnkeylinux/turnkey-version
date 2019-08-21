8b9c0c9bd99b737c4dffe3c268b4b7ce3533b914
Stefan Davis <stefan@turnkeylinux.org>
2019-08-21 02:44:30 +0000

    Turn into transitional package

--- setup.py
+++ setup.py
@@ -6,5 +6,4 @@ setup(name='turnkey-version',
     version='1.0',
     description='Turnkey Version Reporting Utility',
     author='Stefan Davis',
-    url='https://github.com/turnkeylinux/turnkey-version',
-    scripts=['turnkey-version'])
+    url='https://github.com/turnkeylinux/turnkey-version')
